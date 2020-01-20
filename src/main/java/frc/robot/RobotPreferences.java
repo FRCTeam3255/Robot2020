ebb8525903b3413f757b7ee202607faa5424a43a
IanSzalai <60115484+IanSzalai@users.noreply.github.com>
2020-01-20 15:28:40 -0800

    fixed RobotPreferences

--- src/main/java/frc/robot/RobotPreferences.java
+++ src/main/java/frc/robot/RobotPreferences.java
@@ -1,24 +1,14 @@
 /*----------------------------------------------------------------------------*/
-/* Copyright (c) 2019 FIRST. All Rights Reserved.                             */
+/* Copyright (c) 2018-2019 FIRST. All Rights Reserved.                        */
 /* Open Source Software - may be modified and shared by FRC teams. The code   */
 /* must be accompanied by the FIRST BSD license file in the root directory of */
 /* the project.                                                               */
 /*----------------------------------------------------------------------------*/
 
-package frc.robot.subsystems;
+package frc.robot;
 
-import edu.wpi.first.wpilibj2.command.SubsystemBase;
-
-public class RobotPreferences extends SubsystemBase {
-  /**
-   * Creates a new RobotPreferences.
-   */
-  public RobotPreferences() {
-
-  }
-
-  @Override
-  public void periodic() {
-    // This method will be called once per scheduler run
-  }
+/**
+ * Add your docs here.
+ */
+public class RobotPreferences {
 }
