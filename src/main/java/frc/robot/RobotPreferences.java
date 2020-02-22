802df725e53f694b23a8fec9ced29dc49d138bb5
Mike Smith <mssmichaelsmith@gmail.com>
2020-02-22 10:08:49 -0800

    nudgehood feedback and robot prefs for close range shot

--- src/main/java/frc/robot/RobotPreferences.java
+++ src/main/java/frc/robot/RobotPreferences.java
@@ -84,6 +84,7 @@ public class RobotPreferences {
         public static final SN_DoublePreference susanI = new SN_DoublePreference("susanI", 0);
         public static final SN_DoublePreference susanD = new SN_DoublePreference("susanD", 0);
         public static final SN_DoublePreference shooterMaxRPM = new SN_DoublePreference("shooterMaxRPM", 5000);
+        public static final SN_DoublePreference shooterCloseRPM = new SN_DoublePreference("shooterCloseRPM", 4000);
         public static final SN_DoublePreference shooterCloseLowRPM = new SN_DoublePreference("shooterCloseLowRPM", 1000);
         public static final SN_DoublePreference shooterCloseHighRPM = new SN_DoublePreference("shooterCloseHighRPM", 3500);
         public static final SN_DoublePreference shooterP = new SN_DoublePreference("shooterP", 1E-4);
